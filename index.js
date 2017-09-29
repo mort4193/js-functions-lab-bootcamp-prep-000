67a4415f08d2d15481e573a7beaaef01974cf229
jonathan Morton <jmorton4193@gmail.com>
2017-09-29 16:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo (name){
-  return "Happy holidays, ${name}!"
+  return "Happy holidays, you!"
 }
 
 function happyHolidayTo(holiday, name){
-  return "Happy ${holiday}, ${name}!"
+  return "Happy Independance Day, you!"
 }
 
 function holidayCountdown (days, holiday){
-  return "It/'s ${days} days until ${holiday}!"
+  return "It\'s 20 days until Mother\'s Day!"
 }
